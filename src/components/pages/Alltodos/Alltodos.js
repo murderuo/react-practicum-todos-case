48b8a57fc7bc14844dd627238ba5c28403c36ba4
murderuo <ugurokur365@hotmail.com>
2022-09-13 23:13:40 +0300

    add and delete button added

--- src/components/pages/Alltodos/Alltodos.js
+++ src/components/pages/Alltodos/Alltodos.js
@@ -1,13 +1,13 @@
-import MainStyle from '../Main/Main.module.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import MainStyle from "../Main/Main.module.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Alltodos() {
   const [todos, setTodos] = useState([]);
 
   const getData = async () => {
     const response = await axios.get(
-      'https://631eea8322cefb1edc3d783a.mockapi.io/todos',
+      "https://631eea8322cefb1edc3d783a.mockapi.io/todos"
     );
     // console.log(response.data);
     const data = await response.data;
@@ -21,12 +21,18 @@ function Alltodos() {
   return (
     <>
       <div className={MainStyle.container}>
-        <div >
+        <div>
           <ul className={MainStyle.todoContainer}>
             {todos.length === 0 ? (
               <div>Loading</div>
             ) : (
-              todos.map(item => <li key={item.id}>{item.content}</li>)
+              todos.map((item) => (
+                <li key={item.id}>
+                  <input type="checkbox" /> {item.content}
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </li>
+              ))
             )}
             {}
           </ul>
