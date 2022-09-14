fbf3ce74ce88dcbf48cc39012276ea7931b37601
murderuo <ugurokur365@hotmail.com>
2022-09-14 21:43:38 +0300

    portal window task not completed

--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -1,30 +1,37 @@
-import { createPortal } from 'react-dom';
-import MainStyle from '../pages/Main/Main.module.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { createPortal } from "react-dom";
+import MainStyle from "../pages/Main/Main.module.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Portal({ isOpen, setIsOpen, item, setTodoItem }) {
-  console.log(item);
+  // console.log(item);
+  const [loading, setLoading] = useState(false);
   if (!isOpen) return null;
 
   const putData = async () => {
+    setLoading(true);
+    console.log("data fetch start");
+
     const response = await axios.put(
       `https://631eea8322cefb1edc3d783a.mockapi.io/todos/${item.id}`,
-      item,
+      item
     );
     // console.log(response.data);
     const data = await response.data;
+    console.log("data fetch finished");
+    setLoading(false);
     console.log(data);
   };
 
-  const handleUpdate = e => {
+  const handleUpdate = (e) => {
     // console.log(e.target.value);
     setTodoItem({ ...item, [e.target.name]: e.target.value });
   };
 
   const handleSave = () => {
     putData();
-    setIsOpen(false);
+    loading ? setIsOpen(false) : setIsOpen(true);
+    // setIsOpen(false);
   };
 
   return createPortal(
@@ -37,17 +44,19 @@ function Portal({ isOpen, setIsOpen, item, setTodoItem }) {
           onChange={handleUpdate}
           name="content"
         />
-        <label htmlFor="">
+        <label>
           is Completed ?
-          <input type="checkbox" defaultChecked={item.isCompleted} />
+          <input type="checkbox" defaultChecked={item.isCompleted} name='isCompleted' />
         </label>
         <div className="buttons">
-          <button onClick={handleSave}>Save</button>
+          <button onClick={handleSave} disabled={loading}>
+            {loading?'Updating...':'Save'}
+          </button>
           <button onClick={() => setIsOpen(false)}>Close</button>
         </div>
       </div>
     </div>,
-    document.getElementById('portal'),
+    document.getElementById("portal")
   );
 }
 
